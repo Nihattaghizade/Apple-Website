6c78cabaf1d1c1e707f3f53146750be94bbc5cba
Nihattaghizade <nihattagizade2003@gmail.com>
2025-07-06 12:58:31 +0400

    Add text

--- apple__website/src/components/Footer.jsx
+++ apple__website/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
             you.
           </p>
           <p className="font-semibold text-gray text-xs">
-            Or call 000800-040-1966
+            Or call 000000-000-0000
           </p>
         </div>
 
@@ -21,7 +21,7 @@ const Footer = () => {
 
         <div className="flex md:flex-row flex-col md:items-center justify-between">
           <p className="font-semibold text-gray text-xs">
-            Copright @ 2024 Apple Inc. All rights reserved.
+            Copright @ Nihat Taghizade. All rights reserved.
           </p>
           <div className="flex">
             {footerLinks.map((link, i) => (
